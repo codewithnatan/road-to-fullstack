efdec6653dcd691bfade7f8e81977b98bcbe5c2b
codewithnatan <natangraboo@gmail.com>
2026-07-26 21:05:05 +0200

    feat: complete vanilla js todo list with reusable counter function

---
+++ 01-javascript-fundamentals/todo-list/app.js
@@ -0,0 +1,53 @@
+// Die Brücke zum HTML
+const meinBtn = document.getElementById('btn');
+const meinInput = document.getElementById('eingabefeld');
+const meineListe = document.getElementById('toDoListe');
+const loeschGnzeListeBtn = document.getElementById('btnLoeschen');
+const zaehler = document.getElementById('aufgabenZahl');
+
+// Funktion die das Zählen der Aufgaben übernimmt
+function zaehlerMaschine() {
+    const zaehler = document.getElementById('aufgabenZahl');
+    zaehler.innerHTML = meineListe.children.length;
+};
+
+// Arrow Funktion für den Button
+meinBtn.addEventListener('click', () => {
+    // Hier bekommt die Konstande text den Wert vom Imput
+    const text = meinInput.value;
+    // Hier gucken wir ob das Input-Feld leer ist
+    if (text === '') {
+        alert("Eingabefeld ist leer!");
+        // Hier ist unser Stoppschiild
+        return;
+    }
+    // Hier erstellen wir eine neue li
+    const neuesLi = document.createElement('li');
+
+    const loeschBtn = document.createElement('button');
+
+    //const anzahl = meineListe.children.length;
+    loeschBtn.innerText = 'X';
+
+    loeschBtn.addEventListener('click', () => {
+        neuesLi.remove();
+
+        zaehlerMaschine();
+    });
+
+    loeschGnzeListeBtn.addEventListener('click', () => {
+        meineListe.innerText = '';
+
+        zaehlerMaschine();
+    });
+
+    // Hier fügen wir der neuen li den Text vom Input-Feld
+    neuesLi.innerText = text;
+    // Hier hengen wir das neue li-Element als Kind in unsere HTML-Liste ein>
+    neuesLi.appendChild(loeschBtn);
+    meineListe.appendChild(neuesLi);
+    zaehlerMaschine();
+    // Hier wird das Input-Feld gelöscht
+    meinInput.value = '';
+
+});
---
+++ README.md
@@ -0,0 +1,13 @@
+# 🗺️ Road to Fullsttack (My Learning Path & Portfolio)
+
+Welcome to my central learning repository! TThis is where I documment my structuured jouurney from core JavaScript fundamentals to production-reeady fullstack niche products.
+
+## 🛠️ How I Learn & Build
+* **Methood:** Using AI as a mentor (grasping logic and syntax examplees through guidance, never copying pre-made solutions).
+* **Focus:** Functtionality and clean logic always come beforeaesthetics.
+
+## 📂 Repository Structure
+
+### 01-javascript-fundamentals/
+* **todo-list/**: My functional web application built entirely with Vanilla JS.
+* **Concepts learned:** DOM manipulation (`getElementById`, `createElement`, `appendChild`, `.remove()`), Event Listeners (`addEventListener`), timing/order of code execution, and code refactoring using reusable functions.
