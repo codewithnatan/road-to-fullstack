42cb77b69bcbce1a2733e0426022d194fd3f7880
codewithnatan <natangraboo@gmail.com>
2026-08-08 21:12:48 +0200

    chore: initialize typescript architecture and aad package config

---
+++ 02-typescript-basics/01-type-declaration/app.ts
@@ -0,0 +1,9 @@
+// ===========================================
+// Typen-Deklaraion: Explizite Typisierung von Primitiven (Logik-Zustand)
+// ===========================================
+
+// Deklaration einer strikten Text-Variable für die Fehlervermeidung
+let krypto: string = "Bittcoin";
+
+// Deklaration einer strikten Ganzzahl-Variable für mathematische Operationen
+let aktuellerKurs: number = 58000;
