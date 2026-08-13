10267ec752881e2789f86c3a8f97f04311bf9521
codewithnatan <natangraboo@gmail.com>
2026-08-13 08:08:41 +0200

    feat: implement v-for list rendering with strict template type checking

--- 03-vue-frontend/01-vue-basics/src/App.vue
+++ 03-vue-frontend/01-vue-basics/src/App.vue
@@ -3,25 +3,32 @@
 // 1. Wir holen das reaktive Werkzeug aus dem Vue-Kern
 import { ref } from 'vue';
 
-// 2. Wir erstellen eine reaktive Variable (Startwert: 0)
-// TypeScript erkennt automatisch: Das ist eine 'number'
-let count = ref(0);
-
-// 3. Eine Funktion, die den Zähler hochzählt
-function erhoeheZaehler(): void {
-  count.value++; // Wichtig: Im TypeScript-Bereich müssen wir immer über '.value' an den Wert ran
+// Interface (Der Bauplan)
+interface CryptoCoin {
+  id: string;
+  name: string;
+  priceEuro: number;
 }
+
+// Ein reaktives Array, das auf unser Interface hört
+// Wir sagen Vue: Diese Liste darf nur Objekte vom Typ 'CryptoCoin' enthalten
+let cryptoWallet = ref<CryptoCoin[]>([
+  {id: "btc", name: "Bitcoin", priceEuro: 58000},
+  {id: "eth", name: "Ethereum", priceEuro: 3100},
+  {id: "doge", name: "Dogecoin", priceEuro: 0.12}
+]);
 </script>
 
 <template>
   <div>
-    <h1>Road to Fullstack: Vue + TypeScript</h1>
+    <h1>Mein Krypto-Wallet (List-Rendering)</h1>
+    <p>Diese Liste wird live aus einem TypeScript-Array generiert:</p>
 
-    <!-- Hier zeigen wir den reaktiven Wert live an -->
-    <p>Aktueller Klick-Stand: {{ count }}</p>
-
-    <!-- Ein Button, der bei einem Klick (@click) unsere Funktion ausführt -->
-     <button @click="erhoeheZaehler">Klick mich</button>
+    <ul>
+      <!-- v-for schleife durch das Array. :key ist wichtig für Vue -->
+       <li v-for="coin in cryptoWallet" :key="coin.id" class="coin-item"><strong>{{ coin.name }}</strong>: {{ coin.priceEuro }} €
+       </li>
+    </ul>
   </div>
 </template>
 
@@ -30,16 +37,23 @@ function erhoeheZaehler(): void {
 h1 {
   color: #42b883; /* Das Vue Grün */
 }
-button {
-  background-color: #42b883;
-  color: white;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
+ul {
+  list-style: none;
+  padding: 0;
+  width: 300px;
 }
-button:hover {
+p {
+  font-size: 18px;
+  margin-top: 15px;
+}
+.coin-item {
   background-color: #35495e;
+  color: white;
+  margin: 10px 0;
+  padding: 12px;
+  border-radius: 5px;
+  font-size: 18px;
+ display: flex;
+ justify-content: space-between;
 }
 </style>
